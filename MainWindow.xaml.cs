1776a9f3504135e2e45235ea8ecf221d7ce3abad
Anna Kofanova <kofanov4a@yandex.ru>
2026-01-05 23:55:20 +1000

    добавлен GhostBlock

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -86,6 +86,7 @@ namespace Tetris
                 for (int c = 0; c < grid.Columns; c++)
                 {
                     int id = grid[r, c];
+                    imageControls[r, c].Opacity = 1;
                     imageControls[r, c].Source = tileImages[id];
 
                 }
@@ -97,6 +98,7 @@ namespace Tetris
         {
             foreach (Position p in block.TilePositions())
             {
+                imageControls[p.Row, p.Column].Opacity = 1;
                 imageControls[p.Row, p.Column].Source = tileImages[block.Id];
             }
         }
@@ -118,10 +120,21 @@ namespace Tetris
                 HoldImage.Source = blockImages[heldBlock.Id];
             }
         }
+
+        private void DrawGhostBlock(Block block)
+        {
+            int dropDistance = gameState.BlockDropDistance();
+            foreach (Position p in block.TilePositions())
+            {
+                imageControls[p.Row + dropDistance, p.Column].Opacity = 0.25;
+                imageControls[p.Row + dropDistance, p.Column].Source = tileImages[block.Id];
+            }
+        }
         
         private void Draw(GameState gameState)
         {
             DrawGrid(gameState.GameGrid);
+            DrawGhostBlock(gameState.CurrentBlock);
             DrawBlock(gameState.CurrentBlock);
             DrawNextBlock(gameState.BlockQueue);
             DrawHeldBlock(gameState.HeldBlock);
